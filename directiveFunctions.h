03687945bd68ebf892587e55b46555f57e28adbb
Shakked Brand <shaked.brand@gmail.com>
2021-08-07 15:46:32 -0400

    finished documentation for directiveFunctions

--- directiveFunctions.h
+++ directiveFunctions.h
@@ -8,10 +8,40 @@
 #include "dataTable.h"
 #include "entriesOrExternList.h"
 
+/*
+ * handles reading a .dh directive line
+ * accepts the position pointer, the input line string, a pointer to the data counter, an error codes pointer,
+ * data image tail and the current line pointer
+ */
 void dh(int *, const char [], long *, errorCodes *, dataNode**, long);
+/*
+ * handles reading a .dw directive line
+ * accepts the position pointer, the input line string, a pointer to the data counter, an error codes pointer,
+ * data image tail and the current line pointer
+ */
 void dw(int *, const char[], long *, errorCodes *, dataNode**, long);
+/*
+ * handles reading a .db directive line
+ * accepts the position pointer, the input line string, a pointer to the data counter, an error codes pointer,
+ * data image tail and the current line pointer
+ */
 void db(int *, const char [], long *, errorCodes *, dataNode**, long);
+/*
+ * handles reading an .asciz directive line
+ * accepts the position pointer, the input line string, a pointer to the data counter, an error codes pointer,
+ * data image tail and the current line pointer
+ */
 void asciz(int *, const char[], long *,  errorCodes *, dataNode**, long);
+/*
+ * handles reading an .entry directive line
+ * accepts the position pointer, the input line string, an error code pointer, the current line counter, and the
+ * entries/extern linked list
+ */
 void entry(int *, const char[], errorCodes *, long, entriesOrExternList**);
+/*
+ * handles reading an .external directive line
+ * accepts the position pointer, the input line string, the table of symbols, an error code pointer, the current line counter,
+ * and the entries/extern linked list
+ */
 void external(int *, const char[], dataTable *, errorCodes *, long, entriesOrExternList**);
 #endif
