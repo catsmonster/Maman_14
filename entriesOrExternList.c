c5bc06746a18c44d8e06aa7c14441089965f705a
Shakked Brand <shaked.brand@gmail.com>
2021-08-07 18:17:50 -0400

    documented entriesOrExternList.c

--- entriesOrExternList.c
+++ entriesOrExternList.c
@@ -2,6 +2,10 @@
 #include "entriesOrExternList.h"
 #include <stdlib.h>
 
+/*
+ * struct for a linked list containing the names of the entries or external symbols, their type, and the line in which
+ * they were encountered.
+ */
 struct entriesOrExternNode {
     char *name;
     symbolTypes type;
@@ -9,6 +13,9 @@ struct entriesOrExternNode {
     entriesOrExternList *next;
 };
 
+/*
+ * returns an entriesOrExtern list head
+ */
 entriesOrExternList *initializeEntriesOrExternList() {
     entriesOrExternList *p = calloc(1, sizeof(entriesOrExternList));
     if (!p) {
@@ -40,6 +47,9 @@ entriesOrExternList *setNodeTail(entriesOrExternList *tailNode, errorCodes *erro
     return tailNode;
 }
 
+/*
+ * adds a new node to the entriesOrExtern list tail, and returns the new tail
+ */
 entriesOrExternList *setEntryOrExternListTail(char *name, symbolTypes type, entriesOrExternList *tail, errorCodes *error, long currentLine) {
     tail = setNodeTail(tail, error);
     tail -> type = type;
@@ -48,25 +58,39 @@ entriesOrExternList *setEntryOrExternListTail(char *name, symbolTypes type, entr
     return tail;
 }
 
+/*
+ * returns the name of the entry/extern label of the node
+ */
 char *getEntryOrExternName(entriesOrExternList *node) {
     return node -> name;
 }
 
+/*
+ * returns the type of the symbol of the label of the node
+ */
 symbolTypes getEntryOrExternType(entriesOrExternList *node) {
     return node -> type;
 }
 
+/*
+ * returns the next linked node to the current node
+ */
 entriesOrExternList *getNextEntryOrExtern(entriesOrExternList *node) {
     return node -> next;
 }
 
+/*
+ * returns the line in which the label was added to the list
+ */
 long getEntryOrExternLine(entriesOrExternList *node) {
     return node -> currentLine;
 }
 
 
 
-
+/*
+ * frees the list of entry and extern labels.
+ */
 void freeEntriesOrExternList(entriesOrExternList *head) {
     while (head) {
         entriesOrExternList *temp = head;
